b7c399bda3df7945d0cee8e22994a735020fd033
Phillip Henry <PhillHenry@gmail.com>
2021-12-19 09:26:14 +0000

    Trivial refactor.

--- src/main/scala/uk/co/odinconsultants/ecocats3/MyMain.scala
+++ src/main/scala/uk/co/odinconsultants/ecocats3/MyMain.scala
@@ -3,6 +3,7 @@ package uk.co.odinconsultants.ecocats3
 import cats.effect.{Concurrent, IO, IOApp}
 import cats.implicits.*
 import fs2.Stream
+import fs2.Chunk
 import io.circe.{Decoder, Encoder}
 import org.http4s.*
 import org.http4s.Method.*
@@ -17,20 +18,25 @@ import org.http4s.client.Client
 object MyMain extends IOApp.Simple {
   val dsl = new Http4sClientDsl[IO]{}
   import dsl.*
+
+  val Request: Request[IO] = GET(uri"https://icanhazdadjoke.com/")
+
   def run: IO[Unit] = {
     val stream: Stream[IO, Client[IO]] = Stream.resource(EmberClientBuilder.default[IO].build)
-    val call = stream.flatMap { (client: Client[IO]) =>
-      println(s"client = $client")
-      val request: Request[IO] = GET(uri"https://icanhazdadjoke.com/")
-      val joke: Stream[IO, Response[IO]] = client.stream(request)
-      val printed = joke.flatMap { response =>
-        response.body.chunks.flatMap { chunk =>
-          Stream.eval(IO(print(new String(chunk.toArray))))
-        }
-      }.handleError(t => t.printStackTrace())
-      printed
-    }
+    val call = stream.flatMap { streamChunks(Request, printChunk) }
     val output = call.compile.drain
     output
   }
+
+  def streamChunks(request: Request[IO], chunking: ChunkFunc): Client[IO] => Stream[IO, Unit] =
+    client =>
+      val joke: Stream[IO, Response[IO]] = client.stream(request)
+      val printed: Stream[IO, Unit] = joke.flatMap { response =>
+        response.body.chunks.flatMap(chunking)
+      }.handleError(t => t.printStackTrace())
+      printed
+
+  type ChunkFunc = Chunk[Byte] => Stream[IO, Unit]
+
+  val printChunk: ChunkFunc = chunk => Stream.eval(IO(print(new String(chunk.toArray))))
 }
