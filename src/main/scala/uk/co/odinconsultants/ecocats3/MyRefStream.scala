6d1184958bd17442d046146c3cd65d2ee33bc1d1
Phillip Henry <PhillHenry@gmail.com>
2022-01-03 13:00:17 +0000

    Need to eval to get the Reg (not emit).

--- src/main/scala/uk/co/odinconsultants/ecocats3/MyRefStream.scala
+++ src/main/scala/uk/co/odinconsultants/ecocats3/MyRefStream.scala
@@ -7,9 +7,9 @@ object MyRefStream {
 
   def stateStream: Stream[IO, Boolean] = {
     for {
-      io <- Stream.emit(Ref.of[IO, Boolean](true))
+      ref <- Stream.eval(Ref.of[IO, Boolean](true))
       values <- Stream.emits(Seq(1,2,3))
-      changed <- Stream.eval(io.flatMap(ref => ref.updateAndGet(_ => values % 2 == 0)))
+      changed <- Stream.eval(ref.updateAndGet(x => !x))
     } yield {
       println(s"changed = $changed")
       changed
