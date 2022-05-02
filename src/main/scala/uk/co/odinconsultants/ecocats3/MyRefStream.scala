e073bdd21031c02299a8549d2f7bf8d2e3c33e25
Phillip Henry <PhillHenry@gmail.com>
2022-05-02 09:33:49 +0100

    Documentation.

--- src/main/scala/uk/co/odinconsultants/ecocats3/MyRefStream.scala
+++ src/main/scala/uk/co/odinconsultants/ecocats3/MyRefStream.scala
@@ -7,9 +7,9 @@ object MyRefStream {
 
   def stateStream: Stream[IO, Boolean] =
     for {
-      ref     <- Stream.eval(Ref.of[IO, Boolean](true))
-      x       <- Stream.emits(Seq(1,2,3))
-      changed <- Stream.eval(ref.updateAndGet(x => !x))
+      ref:     Ref[IO, Boolean] <- Stream.eval(Ref.of[IO, Boolean](true)) // note `Ref.of` returns F[Ref[F, A]]. `eval` gets inside the burrito
+      x:       Int              <- Stream.emits(Seq(1,2,3))
+      changed: Boolean          <- Stream.eval(ref.updateAndGet(x => !x)) // note: the *result* of *eval*uating the Ref. Again, we get inside the `F`
     } yield {
       println(s"$x: changed = $changed")
       changed
