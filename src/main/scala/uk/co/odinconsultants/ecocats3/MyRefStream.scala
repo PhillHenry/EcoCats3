ada3613c7658ffc8d00ce53f75b449394eea76c5
Phillip Henry <PhillHenry@gmail.com>
2022-05-02 09:25:04 +0100

    Formatting.

--- src/main/scala/uk/co/odinconsultants/ecocats3/MyRefStream.scala
+++ src/main/scala/uk/co/odinconsultants/ecocats3/MyRefStream.scala
@@ -5,15 +5,14 @@ import fs2.Stream
 
 object MyRefStream {
 
-  def stateStream: Stream[IO, Boolean] = {
+  def stateStream: Stream[IO, Boolean] =
     for {
-      ref <- Stream.eval(Ref.of[IO, Boolean](true))
-      x <- Stream.emits(Seq(1,2,3))
+      ref     <- Stream.eval(Ref.of[IO, Boolean](true))
+      x       <- Stream.emits(Seq(1,2,3))
       changed <- Stream.eval(ref.updateAndGet(x => !x))
     } yield {
       println(s"$x: changed = $changed")
       changed
     }
-  }
 
 }
