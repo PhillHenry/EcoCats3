5998107d16a79ed7ae09b0da48b6d42721abbb46
Phillip Henry <PhillHenry@gmail.com>
2022-06-13 15:00:55 +0100

    Comments.

--- src/main/scala/uk/co/odinconsultants/ecocats3/StateExample.scala
+++ src/main/scala/uk/co/odinconsultants/ecocats3/StateExample.scala
@@ -2,6 +2,22 @@ package uk.co.odinconsultants.ecocats3
 
 import cats.data.State
 
+/**
+  * thanh — 06/11/2022
+Yes, of course, I'm working on a Repl for an lambda calculus interpreter. Just using readLine and println at the moment (so without cats-effect IO).
+It works fine until I want to add an Env which is just a Map[String, Expr] which I want to use StateMonad to store that data
+I did make an example with State like this
+  * SystemFw — 06/11/2022
+yeah, embedding side-effects directly into State is going to be pretty confusing
+  I mean, for real code I'd recommend IO and Ref without State, yes
+ofc, there are various things you can explore that might be instructive
+because as far as I can see there is a fundamental misunderstanding in that example
+which is that the whole loop needs to be in State if you want it to be stateful across iterations
+  in particular you need to find a way for println and readLine to return State
+you can do that by embedding the side effects in there (frowned upon, but it's just for learning)
+or by converting the whole thing to StateT[IO (works, but it's kinda complicated)
+or by ditching State in favour of Ref, and sticking to IO
+  */
 object StateExample {
   type Env = Map[String, Int]
 
